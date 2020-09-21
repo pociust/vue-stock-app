bebf21679f424d458ae1f728fbc42cb896a75e89
Tom <tom@pop-os.localdomain>
2020-09-21 15:47:52 -0500

    init vue project, npm i and connected intro template

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>name: {{ title }} </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        title: 'Vue Stock App'
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  div {
+    color: red;
+  }
+</style>
