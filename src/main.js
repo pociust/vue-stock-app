926d09e10a232990329763ae3f1aeefc6196ec81
Tom <tom@pop-os.localdomain>
2020-09-21 16:31:44 -0500

    imported vuerouter and boilerplate set up router in vue instance

--- src/main.js
+++ src/main.js
@@ -1,11 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
-
+import { routes } from './router.js'
 import { store } from './store/store.js';
+
+Vue.use(VueRouter);
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+})
 
 
 new Vue({
   el: '#app',
+  router,
   store,
   render: h => h(App)
-})
+});
