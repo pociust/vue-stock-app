393fa7a0469f92bd94948d5380fe013b8003677d
Tom <tom@pop-os.localdomain>
2020-09-21 16:18:11 -0500

    added components that will be used including pages

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <div>
+      <div class="left-side-nav">
+        <div>Stock Tracker</div>
+        <div>Portfolio</div>
+        <div>Stocks</div>
+      </div>
+      <div class="right-side-nav">
+        <div>End Day</div>
+        <div>Save & Load </div>
+        <div>Funds: $10,000</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+  
+  }
+</script>
