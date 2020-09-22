36ce03390a304446c7f678ba1a2b46956f721e70
Tom <tom@pop-os.localdomain>
2020-09-21 19:01:34 -0500

    import vue resource into main js and added root url for database

--- src/main.js
+++ src/main.js
@@ -1,10 +1,17 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource'
 import App from './App.vue'
+
 import { routes } from './router.js'
 import { store } from './store/store.js';
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
+
+Vue.http.options.root = 'https://vue-intro-firebase.firebaseio.com/';
+
+
 const router = new VueRouter({
   routes,
   mode: 'history'
