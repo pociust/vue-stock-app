1bf40c30d9e0acff23db3aded0f12aafa8123de2
Tom <tom@pop-os.localdomain>
2020-09-23 22:37:54 -0500

    set company as a prop and created computed to set company full name

--- src/components/StockCard.vue
+++ src/components/StockCard.vue
@@ -1,5 +1,37 @@
 <template>
   <div>
-    <div>Stock Card</div>
+    <div>{{ companyName }} ({{ company.name }}) </div>
   </div>
 </template>
+<script>
+  export default {
+    data () {
+      return {
+        name: '',
+      }
+    },
+    props: {
+      company: {
+        type: Object,
+      }
+    },
+    computed: {
+      companyName() {
+        let companyName = ''
+        if (this.company.name === "MSFT") {
+          companyName = "Microsoft";
+        } else if ( this.company.name === "GOOGL") {
+          companyName = "Google"
+        } else if (this.company.name === "TSLA") {
+          companyName = "Tesla"
+        } else if (this.company.name === "AAPL") {
+          companyName = "Apple"
+        }
+
+        return companyName
+        
+      }
+    }
+  }
+</script>
+
