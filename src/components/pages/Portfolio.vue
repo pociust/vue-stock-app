02f4a1c3d4e81e2536b7009fb3fa1e8b36006aee
pociust <poci14@gmail.com>
2020-09-27 16:04:35 -0500

    added portfolio page with get route to display stocks saved in db

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -1,13 +1,34 @@
 <template>
   <div>
-    <div>
-      this is the portfolio page!
+    <div v-for="stock in stocks" :key="stock.symbol">
+      <div>
+        this is the portfolio page! {{ stock.symbol }}
+      </div>
+      
     </div>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        stocks: [],
+      }
+    },
+    created() {
+      this.$http.get('data.json')
+        .then(response => {
+          return response.json();
+        })
+        .then( data => {
+          const resultArray = [];
+          for(let symbol in data) {
+            resultArray.push(data[symbol]);
+          }
+          this.stocks = resultArray;
+        })
+    }
   
   }
 </script>
