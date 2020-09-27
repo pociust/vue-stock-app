2022904b2b9f5b412e05587b242353c24088f110
pociust <poci14@gmail.com>
2020-09-27 18:17:13 -0500

    added sell button with styling

--- src/components/PortfolioCard.vue
+++ src/components/PortfolioCard.vue
@@ -3,8 +3,11 @@
     <div class="portfolio-card-key">
       {{ stockKey }}
     </div>
-    <div class="shadow-light p-5 m-5" v-for="info in stockInfo" :key="info.id">
+    <div class="shadow-light p-5 m-5 frow justify-around" v-for="info in stockInfo" :key="info.id">
+      <div class="frow items-center">
       {{ info.data.amount }} {{ info.data.amount > 1 ? `shares` : `share` }} at ${{ info.data.price }}
+      </div>
+      <div class="sell-button">Sell</div>
     </div>
   </div>
 </template>
@@ -24,5 +27,16 @@
 <style scoped>
   .portfolio-card-key {
     font-size: 50px;
+  }
+
+  .sell-button {
+    padding: 5px;
+    border: 1px solid var(--hover-purple);
+    border-radius: 10px;
+  }
+
+  .sell-button:hover{
+    cursor: pointer;
+    background-color: var(--main-purple);
   }
 </style>
