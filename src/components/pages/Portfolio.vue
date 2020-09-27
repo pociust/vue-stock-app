894750bbda2a262e0a536cdec17fb7ac85213728
pociust <poci14@gmail.com>
2020-09-27 17:03:40 -0500

    created result object to sort each stock and push into an array to be displayed

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -1,20 +1,24 @@
 <template>
-  <div>
-    <div v-for="stock in stocks" :key="stock.id">
-      <div>
-        this is the portfolio page! {{ stock.data.funds }}
+  <div class="frow">
+    <div class="items-stretch shadow-dark m-20 p-10" v-for="stock in stocks" :key="stock.id">
+      <div>{{ stock }} </div>
+      <div class="col-md-1-4">
+        <PortfolioCard :stockInfo="stock.data"></PortfolioCard>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
+  import PortfolioCard from '../PortfolioCard.vue'
   export default {
     data() {
       return {
         stocks: [],
       }
+    },
+    components: {
+      PortfolioCard,
     },
     created() {
       this.$http.get('data.json')
@@ -22,11 +26,38 @@
           return response.json();
         })
         .then( data => {
-          const resultArray = [];
+          let resultObject = {};
+          const teslaArray = [];
+          const microsoftArray = [];
+          const appleArray = [];
+          const googleArray = [];
+
           for(let id in data) {
-          resultArray.push({id: id, data: data[id]});
+
+            if(data[id].symbol === "TSLA"){
+              teslaArray.push({id: id, data: data[id]});
+            }
+             else if(data[id].symbol === "MSFT"){
+              microsoftArray.push({id: id, data: data[id]});
+            }
+             else if(data[id].symbol === "AAPL"){
+              appleArray.push({id: id, data: data[id]});
+            }
+             else if(data[id].symbol === "GOOGL"){
+              googleArray.push({id: id, data: data[id]});
+            }
+
           }
-          this.stocks = resultArray;
+          resultObject = {
+            tesla: teslaArray,
+            microsoft: microsoftArray,
+            apple: appleArray,
+            google: googleArray,
+
+          };
+
+          this.stocks = resultObject;
+
         })
     }
   
