aa9a60441f58de2f2b2b10ed3f4117ec3b85b1e6
pociust <poci14@gmail.com>
2020-10-01 21:49:55 -0500

    added minimum function to display 2 digits

--- src/utils/moneyFilter.js
+++ src/utils/moneyFilter.js
@@ -3,6 +3,6 @@ import Vue from 'vue';
 
 
 Vue.filter('moneyFilter', funds => {
-    return funds ? `$${funds.toLocaleString('en-US', {maximumFractionDigits:2})}` : ``
+    return funds ? `$${funds.toLocaleString('en-US', {maximumFractionDigits: 2, minimumFractionDigits: 2})}` : ``
 
   });
