70c5201f3a6c74e39a45f6c1d570ce7fe1a9f678
pociust <poci14@gmail.com>
2020-10-07 19:59:03 -0500

    changed stocks to better represent data as stock portfolio obtained from the db, added method for getting current stock info to display current price

--- src/components/pages/Portfolio.vue
+++ src/components/pages/Portfolio.vue
@@ -1,13 +1,14 @@
 <template>
   <div class="frow">
     <div
-      v-for="(stock, key) in stocks"
+      v-for="(stock, key) in stockPortfolio"
       :key="stock.id"
       class="portfolio-key-shadow p-5 m-5 col-md-1-5"
     >
       <PortfolioCard
         :stock-info="stock"
         :stock-key="key"
+        :current-price="currentStockInfo"
       />
     </div>
   </div>
@@ -15,47 +16,65 @@
 
 <script>
 import PortfolioCard from '../PortfolioCard.vue';
+import { stockApi } from '@/store/stockApi.js';
 export default {
   components: { PortfolioCard },
   data() {
-    return { stocks: {} };
+    return {
+      stockPortfolio: {},
+      currentStockInfo:{},
+    };
   },
   created() {
-    this.$http.get('data.json')
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        let resultObject = {};
-        const teslaArray = [];
-        const microsoftArray = [];
-        const appleArray = [];
-        const googleArray = [];
+    this.getUserPortfolio();
+    this.getCurrentStockInfo();
+  },
+  methods: {
+    getUserPortfolio() {
+      this.$http.get('data.json')
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let resultObject = {};
+          const teslaArray = [];
+          const microsoftArray = [];
+          const appleArray = [];
+          const googleArray = [];
 
-        for (let id in data) {
-          if (data[id].symbol === 'TSLA'){
-            teslaArray.push({ id: id, data: data[id] });
+          for (let id in data) {
+            if (data[id].symbol === 'TSLA'){
+              teslaArray.push({ id: id, data: data[id] });
+            }
+            else if (data[id].symbol === 'MSFT'){
+              microsoftArray.push({ id: id, data: data[id] });
+            }
+            else if (data[id].symbol === 'AAPL'){
+              appleArray.push({ id: id, data: data[id] });
+            }
+            else if (data[id].symbol === 'GOOGL'){
+              googleArray.push({ id: id, data: data[id] });
+            }
           }
-          else if (data[id].symbol === 'MSFT'){
-            microsoftArray.push({ id: id, data: data[id] });
-          }
-          else if (data[id].symbol === 'AAPL'){
-            appleArray.push({ id: id, data: data[id] });
-          }
-          else if (data[id].symbol === 'GOOGL'){
-            googleArray.push({ id: id, data: data[id] });
-          }
-        }
-        resultObject = {
-          tesla: teslaArray,
-          microsoft: microsoftArray,
-          apple: appleArray,
-          google: googleArray,
+          resultObject = {
+            tesla: teslaArray,
+            microsoft: microsoftArray,
+            apple: appleArray,
+            google: googleArray,
+          };
 
-        };
-
-        this.stocks = resultObject;
+          this.stockPortfolio = resultObject;
+        });
+    },
+    getCurrentStockInfo() {
+      let companies = ['MSFT', 'AAPL', 'GOOGL', 'TSLA'];
+      companies.forEach((company) => {
+        stockApi(company)
+          .then((response) => {
+            this.$set(this.currentStockInfo, response.symbol, response.currentPrice);
+          });
       });
+    },
   },
 };
 </script>
